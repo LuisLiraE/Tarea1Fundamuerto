21e7911edac397ee5777951c6ee57063156cf213
Luis Lirae <luis.lira2401@alumnos.ubiobio.cl>
2026-05-03 16:32:40 -0400

    avance

--- src/ProcesadorAutomata.java
+++ src/ProcesadorAutomata.java
@@ -1,5 +1,20 @@
+import Controlador.Automata;
+
 import java.util.HashMap;
 import java.util.Set;
 
 public class ProcesadorAutomata {
+
+    public static Automata convertirAFNDaAFD(Automata afnd) {
+    return afnd
+    }
+
+    public static Automata minimizarAFD(Automata afd) {
+    return afd
+    }
+
+    public static boolean sonEquivalentes(Automata a1, Automata a2) {
+    return true;
+    }
+
 }
